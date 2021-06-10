3a2d50b78f11e31cbb87c72eb17880031b7f9ca0
ydroulis <lombardi.droulis@gmail.com>
2021-06-10 10:29:57 -0300

    create new component campointeiro

---
+++ src/componentes/CampoInteiro/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { TextInput } from "react-native";
+import estilosPadrao from "./estilos";
+
+export default function CampoInteiro({ valor, estilos, acao }) {
+  const atualiza = (novoValor, acaoRetorno) => {
+    const verificaInteiro = novoValor.match(/^[0-9]*$/);
+
+    if (!verificaInteiro) return;
+
+    const removeZeroEsquerda = novoValor.replace(/^(0)(.+)/, "$2");
+
+    acaoRetorno(removeZeroEsquerda);
+  };
+
+  const numeroEmTexto = String(valor);
+
+  return (
+    <TextInput
+      style={[estilosPadrao.campo, estilos]}
+      keyboardType="number-pad"
+      selectTextOnFocus
+      value={numeroEmTexto}
+      onChangeText={(novoValor) => atualiza(novoValor, acao)}
+    />
+  );
+}
